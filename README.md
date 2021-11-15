90cd54649a7f0116215afd9a43275b645ff9ce71
mglancy <glancy@gmail.com>
2021-11-15 22:26:02 +0000

    Update to kit v11 and add main pages.

--- README.md
+++
@@ -1,2 +0,0 @@
-# how-many-in-uk
-A simple site to calculate how many users of a service have disabilities. Based on the GOV.UK Prototype Kit.
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,82 @@
 const express = require('express')
 const router = express.Router()
 
+// Define percentages for each disability
+
+// UK population: 67 million
+
+// Wear glasses or contact lenses: 49 million 74%
+const glassesContactsPC = 0.74
+
+// Have a disability: 13 million 19%
+const disabledTotalPC = 0.19
+
+// Deaf or hard of hearing: 11 million 16%
+const deafPC = 0.16
+
+// Dyslexia: 6.3 million 9%
+const dyslexiaPC = 0.09
+
+// Colour blindness: 3 million 4.5% (1 in 12 male and 1 in 200 female)
+const colourBlindnessPC = 0.045
+
+// Blind or partially sighted: 2 million 3%
+const blindPC = 0.03
+
+// Autistic spectrum: 700,000 1%
+const autisticPC = 0.01
+
+// BSL users 151,000 0.2%
+const bslPC = 0.002
+
+
 // Add your routes here - above the module.exports line
+
+router.get('/index', function(req, res) {
+    res.render('index', {
+      'errorMessage' : null,
+      'errorTitle' : "",
+      'errorSummary' : false
+    })
+});
+
+router.get('/results', function(req, res) {
+
+    var peopletotal = parseInt(req.query.peopletotal, 10)
+
+    // Check if it's not a number
+    if(isNaN(peopletotal)){
+      res.render('index', {
+        'errorMessage' : {text:"Enter a number"},
+        'errorTitle' : "Error - ",
+        'errorSummary' : true
+      })
+    } else if (peopletotal>68000000){
+      res.render('index', {
+        'errorMessage' : {text:"Enter less than the UK population"},
+        'errorTitle' : "Error - ",
+        'errorSummary' : true
+      })
+    } else if (peopletotal<101){
+      res.render('index', {
+        'errorMessage' : {text:"Sorry, less than 100 users is too few to estimate"},
+        'errorTitle' : "Error - ",
+        'errorSummary' : true
+      })
+    } else {
+      res.render('results', {
+        'peopleTotalDisplay' : peopletotal.toLocaleString('en'),
+        'glassesContacts' : Math.ceil(peopletotal * glassesContactsPC).toLocaleString('en'),
+        'disabledTotal' : Math.ceil(peopletotal * disabledTotalPC).toLocaleString('en'),
+        'deaf' : Math.ceil(peopletotal * deafPC).toLocaleString('en'),
+        'dyslexia' : Math.ceil(peopletotal * dyslexiaPC).toLocaleString('en'),
+        'colourBlindness' : Math.ceil(peopletotal * colourBlindnessPC).toLocaleString('en'),
+        'blind' : Math.ceil(peopletotal * blindPC).toLocaleString('en'),
+        'autistic' : Math.ceil(peopletotal * autisticPC).toLocaleString('en'),
+        'bsl' : Math.ceil(peopletotal * bslPC).toLocaleString('en')
+      })
+    }
+
+});
 
 module.exports = router
